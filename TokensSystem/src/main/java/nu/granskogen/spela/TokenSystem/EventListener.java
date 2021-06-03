2500c429c378be41801da88948a401c843193078
BingooDev <bingooDeveloper@gmail.com>
2021-06-03 09:49:10 +0200

    Added VotifierEvent to give VoteTokens to users who vote

--- TokensSystem/src/main/java/nu/granskogen/spela/TokenSystem/EventListener.java
+++ TokensSystem/src/main/java/nu/granskogen/spela/TokenSystem/EventListener.java
@@ -1,8 +1,12 @@
 package nu.granskogen.spela.TokenSystem;
 
 import org.black_ixx.bossshop.events.BSRegisterTypesEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+
+import com.vexsoftware.votifier.model.VotifierEvent;
 
 public class EventListener implements Listener {
 
@@ -10,5 +14,13 @@ public class EventListener implements Listener {
 	public void onRegisterType(BSRegisterTypesEvent event) {
 		new BSVoteTokenPriceType().register();
 		new BSJobsTokenPriceType().register();
+	}
+	
+	@EventHandler
+	public void onVote(VotifierEvent event) {
+		@SuppressWarnings("deprecation")
+		OfflinePlayer player = Bukkit.getOfflinePlayer(event.getVote().getUsername());
+		Main.getInstance().getVoteToken(player.getUniqueId()).add(1);
+		System.out.println("VoteToken added to " + event.getVote().getUsername());
 	}
 }
